5f73a900e96a7a335664564d18b1e7b6ec8edc8d
Diego Nicolas Larenas  March <dlarenas@Empresas-FPY.Central>
2022-05-02 13:15:33 -0400

    test
    test

--- APITestJenkins/Controllers/WeatherForecastController.cs
+++ APITestJenkins/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace APITestJenkins.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Chill2"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
